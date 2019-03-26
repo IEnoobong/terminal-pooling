d7002329f2bf5d888e487df8bf86e39f88207392
Ibanga Enoobong Ime <ibangaenoobong@yahoo.com>
2019-03-26 18:13:31 +0100

    use external values for scheduler

---
+++ discovery-server/src/main/java/co/enoobong/DiscoveryServerApp.java
@@ -0,0 +1,13 @@
+package co.enoobong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApp {
+  public static void main(String[] args) {
+    SpringApplication.run(DiscoveryServerApp.class, args);
+  }
+}
--- terminal-server/src/main/java/co/enoobong/TerminalServerApp.java
+++ terminal-server/src/main/java/co/enoobong/TerminalServerApp.java
@@ -4,8 +4,10 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TerminalServerApp {
 
   public static void main(String[] args) {
